bd5fa27ff922dd73485515ed5d3c95ab4d401990
Katerina_Charakhovich <v-kcharakhovich@expediagroup.com>
2023-04-10 21:43:50 +0200

    Initial commit

---
+++ src/main/java/com/example/gcpapp/GcpAppApplication.java
@@ -0,0 +1,19 @@
+package com.example.gcpapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GcpAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpAppApplication.class, args);
+	}
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hi there, this is a Google Cloud";
+	}
+}
